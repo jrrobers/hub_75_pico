e02cde773a55c6c5dfe9baffae93c44758e67f2c
joey <joeyrobers07@gmail.com>
2026-08-07 20:41:59 -0400

    Sanitize DATABASE_URL in backend

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ app = FastAPI(
 
 # Railway injects the DATABASE_URL environment variable automatically if connected to a Postgres service
 DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgresql+psycopg2://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql+psycopg2://", "postgresql://", 1)
+
 
 @app.get("/")
 def read_root():
